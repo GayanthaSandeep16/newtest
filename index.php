17cec9a90e10c745a92bc7cc3c4ec6a47c2bacc3
Gayantha <99648217+GayanthaSandeep16@users.noreply.github.com>
2023-03-09 12:02:14 +0530

    Update index.php
    add some data

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
    if( $_GET["name"] || $_GET["age"] ) {
       echo "Welcome ". $_GET['name']. "<br />";
       echo "You are ". $_GET['age']. " years old.";
+      echo"hello"
       
       exit();
    }
